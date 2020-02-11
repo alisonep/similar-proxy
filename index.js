882bbb3f99d9e26ea1e0d012256ffb9c24c139ad
alisonep <alison@enrichconsulting.com>
2020-02-10 19:27:46 -0800

    set up proxy with similar service

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.listen(port, () => console.log('Similar proxy listening on port ' + port));
