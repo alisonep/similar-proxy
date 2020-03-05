d0b9b67ec93536b03f9233569d29f9fe7ab6c664
alisonep <alison@enrichconsulting.com>
2020-03-04 18:25:46 -0800

    add google font, command to start all servers

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 const cors = require('cors');
 const port = 3000;
 
 app.use(express.static('public'));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'/public/index.html'));
+});
+
 app.listen(port, () => console.log('Similar proxy listening on port ' + port));
+
